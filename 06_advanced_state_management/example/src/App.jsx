6c94093f9d2804324412bfbaf34b2caf4f8bf0de
tom474 <emsignouc0407@gmail.com>
2024-08-11 22:08:56 +0700

    Complete Week 6

---
+++ 06_advanced_state_management/example/src/App.jsx
@@ -0,0 +1,12 @@
+import ProductList from "./features/products/ProductList";
+import ShoppingCart from "./features/cart/Cart";
+import "./App.css";
+
+export default function App() {
+	return (
+		<>
+			<ShoppingCart />
+			<ProductList />
+		</>
+	);
+}
